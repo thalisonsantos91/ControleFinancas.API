d4e7742170ab83b1ba5e9d61f47a76d2a7db5874
Thalison Santos <THALISONCAMPELO@HOTMAIL.COM>
2024-03-10 15:36:39 -0300

    Fazendo as primeiras configurações da api, adicionando referências e comunicações entre os projetos.

--- src/ControleFinancas.API/Program.cs
+++ src/ControleFinancas.API/Program.cs
@@ -1,44 +1,145 @@
+using System.Text;
+using AutoMapper;
+using ControleFacil.Api.AutoMapper;
+using ControleFacil.Api.Contract.NaturezaDeLancamento;
+using ControleFacil.Api.Damain.Repository.Classes;
+using ControleFacil.Api.Damain.Repository.Interfaces;
+using ControleFacil.Api.Damain.Services.Classes;
+using ControleFacil.Api.Damain.Services.Interfaces;
+using ControleFacil.Api.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+ConfigurarServices(builder);
+
+ConfigurarInjecaoDeDependencia(builder);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+ConfigurarAplicacao(app);
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+// Metodo que configrua as injeções de dependencia do projeto.
+static void ConfigurarInjecaoDeDependencia(WebApplicationBuilder builder)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    string? connectionString = builder.Configuration.GetConnectionString("PADRAO");
+    
+    builder.Services.AddDbContext<ApplicationContext>(options =>
+        options.UseNpgsql(connectionString), ServiceLifetime.Transient, ServiceLifetime.Transient);
+
+    var config = new MapperConfiguration(cfg => {
+        cfg.AddProfile<UsuarioProfile>();
+        cfg.AddProfile<NaturezaDeLancamentoProfile>();
+        cfg.AddProfile<ApagarProfile>();
+        cfg.AddProfile<AreceberProfile>();
+    });
+
+    IMapper mapper = config.CreateMapper();
+
+    builder.Services
+    .AddSingleton(builder.Configuration)
+    .AddSingleton(builder.Environment)
+    .AddSingleton(mapper)
+    .AddScoped<TokenService>()
+    .AddScoped<IUsuarioRepository, UsuarioRepository>()
+    .AddScoped<IUsuarioService, UsuarioService>()
+    .AddScoped<INaturezaDeLancamentoRepository, NaturezaDeLancamentoRepository>()
+    .AddScoped<IService<NaturezaDeLancamentoRequestContract, NaturezaDeLancamentoResponseContract, long>, NaturezaDeLancamentoService>()
+    .AddScoped<IApagarRepository, ApagarRepository>()
+    .AddScoped<IService<ApagarRequestContract, ApagarResponseContract, long>, ApagarService>()
+    .AddScoped<IAreceberRepository, AreceberRepository>()
+    .AddScoped<IService<AreceberRequestContract, AreceberResponseContract, long>, AreceberService>();
+}
+
+// Configura o serviços da API.
+static void ConfigurarServices(WebApplicationBuilder builder)
+{
+
+    builder.Services
+    .AddCors()
+    .AddControllers().ConfigureApiBehaviorOptions(options =>
+    {
+        options.SuppressModelStateInvalidFilter = true;
+    }).AddNewtonsoftJson();
+
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Description = "JTW Authorization header using the Beaerer scheme (Example: 'Bearer 12345abcdef')",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer"
+        });
+
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                Array.Empty<string>()
+            }
+        });
+
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "ControleFacil.Api", Version = "v1" });   
+    });
+
+    builder.Services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+
+    .AddJwtBearer(x =>
+    {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["KeySecret"])),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+}
+
+// Configura os serviços na aplicação.
+static void ConfigurarAplicacao(WebApplication app)
+{
+    // Configura o contexto do postgreSql para usar timestamp sem time zone.
+    AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+    app.UseDeveloperExceptionPage()
+        .UseRouting();
+
+    app.UseSwagger()
+        .UseSwaggerUI(c =>
+        {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ControleFacil.Api v1");
+                c.RoutePrefix = string.Empty;
+        });
+
+    app.UseCors(x => x
+        .AllowAnyOrigin() // Permite todas as origens
+        .AllowAnyMethod() // Permite todos os métodos
+        .AllowAnyHeader()) // Permite todos os cabeçalhos
+        .UseAuthentication();
+
+    app.UseAuthorization();
+
+    app.UseEndpoints(endpoints => endpoints.MapControllers());
+
+    app.MapControllers();
 }
