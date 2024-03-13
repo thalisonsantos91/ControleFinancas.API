a9a76811371b934b73c5cfb33fa01eb6b79153e3
Thalison Santos <THALISONCAMPELO@HOTMAIL.COM>
2024-03-13 19:49:21 -0300

    Criação do context e da classe de usuário.

---
+++ src/ControleFinancas.API/Data/ApplicationContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleFinancas.API.Data
+{
+    public class ApplicationContext
+    {
+        
+    }
+}
