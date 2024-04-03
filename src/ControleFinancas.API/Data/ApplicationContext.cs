f2415e211a1345fe2bc13edb5d0fb1612feb9ff2
Thalison Santos <THALISONCAMPELO@HOTMAIL.COM>
2024-04-03 17:31:38 -0300

    Subindo modificações

--- src/ControleFinancas.API/Data/ApplicationContext.cs
+++ src/ControleFinancas.API/Data/ApplicationContext.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
+using ControleFinancas.API.Damain.Models;
 
 namespace ControleFinancas.API.Data
 {
     public class ApplicationContext : DbContext
     {
-        public DbSet<>
+        public DbSet<Usuario> Usuario {get;set;}
         
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) {}
     }
 }
